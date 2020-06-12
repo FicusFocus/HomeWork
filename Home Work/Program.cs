9dac2edbf69c359adac09a6b8f3a84133bc4cb06
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2020-06-12 22:43:03 +0600

    New Rep

---
+++ Home Work/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Home_Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 38 * 17;
+            int c = (31 – 5 * a) / b;
+            Console.WriteLine(c);
+        }
+    }
+}
