7125738a519d7d5aebbc23ce18300a8b21c8e9d9
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2020-06-14 00:04:28 +0600

    Home work 6

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeWork6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string secondName;
+            string profession;
+            int age;
+
+            Console.Write("Сообщите Ваше имя: ");
+            name = Console.ReadLine();
+            Console.Write("Сообщите Фамилию: ");
+            secondName = Console.ReadLine();
+            Console.Write("Сколько Вам лет: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ваша професия: ");
+            profession = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {secondName} {name}, Вам {age} лет, по профессии Вы {profession}.");
+        }
+    }
+}
