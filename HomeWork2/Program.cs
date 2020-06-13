8cad3ca6b2ca9c35afdecbe14bc64442edb81c48
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2020-06-13 22:52:49 +0600

    Новое новое ДЗ XD

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int goldCount;
+            int diamondCount;
+            int diamondPrice = 5;
+
+            Console.Write("Введите количество золота: ");
+            goldCount = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Какое кол-во кристалов приобретете? ");
+            diamondCount = Convert.ToInt32(Console.ReadLine());
+
+            goldCount -= diamondCount * diamondPrice;
+
+            Console.WriteLine($"Количество кристалов в инвентаре - {diamondCount}. Количество золота в инвентаре - {goldCount}");
+        }
+    }
+}
