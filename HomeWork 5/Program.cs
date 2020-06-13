3d43ea0258edc6c41453c994dc67e11c54a17c7a
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2020-06-13 23:29:51 +0600

    HomeWork_5

---
+++ HomeWork 5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount;
+            int waitingTimeHour;
+            int waitingTimeMin;
+            int treatmentTime = 10;
+
+            Console.Write("Введите количество людей в очереди: ");
+            peopleCount = Convert.ToInt32(Console.ReadLine());
+
+            waitingTimeHour = peopleCount * treatmentTime / 60;
+            waitingTimeMin = peopleCount * treatmentTime % 60;
+
+            Console.WriteLine($"Время ожидания составит - {waitingTimeHour} часов, {waitingTimeMin} минут");
+        }
+    }
+}
