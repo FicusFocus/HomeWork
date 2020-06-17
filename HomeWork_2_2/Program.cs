ece08021c750346ab5a3726138baa3b78a0bb2be
FicusFocus <shkurihin.pashok@mail.ru>
2020-06-17 19:34:35 +0600

    HomeWork_2_2

---
+++ HomeWork_2_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для отображения списка команд введите help");
+        }
+    }
+}
