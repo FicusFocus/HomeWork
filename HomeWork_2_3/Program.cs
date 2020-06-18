d8fac64d70ab20903bebb34f0ffcc43c76d61595
FicusFocus <shkurihin.pashok@mail.ru>
2020-06-18 13:33:58 +0600

    .

--- HomeWork_2_3/Program.cs
+++ HomeWork_2_3/Program.cs
@@ -11,13 +11,25 @@ namespace HomeWork_2_3
             Console.Write("Введите количество итераций: ");
             iterationCount = Convert.ToInt32(Console.ReadLine());
 
-            //while (iterationCount-- > 0)
-            //{
-            //    Console.Write(num + ", ");
-            //    num += 7;
-            //}
+            while (iterationCount-- > 0)
+            {
+                Console.Write(num + ", ");
+                num += 7;
+            }
 
             for (; iterationCount > 0; iterationCount--)
+            {
+                Console.Write(num + ", ");
+                num += 7;
+            }
+
+            for (int i = iterationCount; iterationCount > 0; iterationCount--)
+            {
+                Console.Write(num + ", ");
+                num += 7;
+            }
+
+            for (num = 7; iterationCount > 0; iterationCount--)
             {
                 Console.Write(num + ", ");
                 num += 7;
