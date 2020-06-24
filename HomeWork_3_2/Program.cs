eed6a1f1edd9d91b7d01dbc5458cb5fa383ba036
FicusFocus <shkurihin.pashok@mail.ru>
2020-06-24 12:37:32 +0600

    HomeWork_3_2

--- HomeWork_3_2/Program.cs
+++ HomeWork_3_2/Program.cs
@@ -6,7 +6,39 @@ namespace HomeWork_3_2
     {
         static void Main(string[] args)
         {
+            int[,] array = new int[10, 10];
+            int maxCount = int.MinValue;
 
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(1, 10);
+                    Console.Write(array[i, j] + " ");
+                    if (maxCount < array[i, j])
+                    {
+                        maxCount = array[i, j];
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("максимальное значение = " + maxCount + "\n");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] == maxCount)
+                    {
+                        array[i, j] = 0;
+                    }
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
