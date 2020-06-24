c2350a46dbe7ca4c52f0e681db69e3ef0dd71573
FicusFocus <shkurihin.pashok@mail.ru>
2020-06-24 07:13:02 +0600

    HomeWork_3_1

---
+++ HomeWork_3_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HomeWork_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[,] array = new int[3, 3];
+            int summ = 0;            
+
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(1, 10);
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            int mult = array[0, 0];
+            for (int i = 1; i < array.GetLength(0); i++ )
+            {
+                mult *= array[i,0];
+            }
+
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                summ += array[1, i]; 
+            }
+            Console.WriteLine("сумма - " + summ);
+            Console.WriteLine("Произведение - " + mult);
+        }
+    }
+}
