af8085a6ccf5dc71544b413363793a3422359fde
FicusFocus <shkurihin.pashok@mail.ru>
2020-06-24 17:12:55 +0600

    HomeWork_3_3

---
+++ HomeWork_3_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HomeWork_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrey = new int[30];
+            Random rnd = new Random();
+
+            for(int i = 0; i < arrey.Length; i++)
+            {
+                arrey[i] = rnd.Next(0, 100);
+                Console.Write(arrey[i] + " ");
+            }
+            Console.WriteLine();
+
+            for(int i = 0; i < arrey.Length; i++)
+            {
+                if(arrey[i] > arrey[i + 1] && arrey[i + 1] <= arrey.Length)
+                {
+                    Console.WriteLine(arrey[i]);
+                }
+            }
+
+        }
+    }
+}
