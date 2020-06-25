35444e5ddea3cc97e35e0d78d4266d7fe6924394
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2020-06-25 06:41:51 +0600

    HomeWork_3_4

---
+++ HomeWork_3_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HomeWork_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[1];
+            int summ = int.MinValue;
+            bool isWork = true;
+
+            while (isWork)
+            {
+                Console.WriteLine()
+
+                switch (Console.ReadLine())
+                {
+                    case "summ":
+                        break;
+                    case "exit":
+                        break;
+                }
+                    
+            }
+            
+        }
+    }
+}
