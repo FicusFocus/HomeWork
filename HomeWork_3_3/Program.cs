b7a8ce0282675728408e285da2bc6dc5680fc7e7
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2020-06-27 23:56:05 +0600

    Я сделяль)

--- HomeWork_3_3/Program.cs
+++ HomeWork_3_3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HomeWork_3_3
 {
@@ -16,6 +17,10 @@ namespace HomeWork_3_3
             }
             Console.WriteLine();
 
+            if(arrey[0] > arrey[1])
+            {
+                Console.WriteLine(arrey[0]);
+            }
             for(int i = 1; i < arrey.Length - 1; i++)
             {
                 if(arrey[i] > arrey[i + 1] && arrey[i] > arrey[i - 1])
@@ -23,7 +28,10 @@ namespace HomeWork_3_3
                     Console.WriteLine(arrey[i]);
                 }
             }
-
+            if (arrey[arrey.Length - 1] > arrey[arrey.Length - 2])
+            {
+                Console.WriteLine(arrey[arrey.Length - 1]);
+            }
         }
     }
 }
