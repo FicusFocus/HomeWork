1e9ae378423196c7ceb5129eac0c7c17457cda7e
FicusFocus <shkurihin.pashok@mail.ru>
2020-06-30 00:41:38 +0600

    HomeWork_2_8

---
+++ HomeWork_2_8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HomeWork_2_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+
+            while (isWork)
+            {
+                Console.WriteLine("Введите exit для выхода");
+
+                switch (Console.ReadLine())
+                {
+                    case "exit":
+                        isWork = false;
+                        break;
+                    default:
+                        Console.WriteLine("все еще работает");
+                        Console.ReadKey();
+                        break;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
