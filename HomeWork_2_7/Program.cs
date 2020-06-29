0b9e857c309e0a0321a2c233eb4fda7373a3a97b
FicusFocus <shkurihin.pashok@mail.ru>
2020-06-30 00:35:18 +0600

    HomeWork_2_7

---
+++ HomeWork_2_7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork_2_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userMassage;
+            int outputCount;
+
+            Console.Write("Введите сообщение: ");
+            userMassage = Console.ReadLine();
+            Console.Write("\nСколько раз вывести сообщение: ");
+            outputCount = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < outputCount; i++)
+            {
+                Console.WriteLine((i + 1) + ") " + userMassage);
+            }
+        }
+    }
+}
