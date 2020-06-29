ed15ee6a54ae3a42fb11c0203610f38320af55c6
FicusFocus <shkurihin.pashok@mail.ru>
2020-06-30 00:24:29 +0600

    HomeWork_3_4

--- HomeWork_3_4/Program.cs
+++ HomeWork_3_4/Program.cs
@@ -7,8 +7,7 @@ namespace HomeWork_3_4
         static void Main(string[] args)
         {
             int[] numbers = new int[1];
-            int[] tempNumber = new int[1];
-            int summ = int.MinValue;
+            int summ = 0;
             string userInput;
             bool isWork = true;
 
@@ -16,23 +15,44 @@ namespace HomeWork_3_4
             {
                 Console.Write("Введите команду.\n" +
                               "1) summ - вывести сумму введенных чиселю\n" +
-                              "2) enter - ввести числа\n" +
+                              "2) enter - ввести число\n" +
                               "3) exit - выйти из программы.\n");
                 userInput = Console.ReadLine();
                 switch (userInput)
                 {
                     case "enter":
-
+                        int[] tempNumbers = new int[numbers.Length + 1];
+                        if (numbers[0] == 0)
+                        {
+                            Console.Write("Введите первое число: ");
+                            numbers[0] = Convert.ToInt32(Console.ReadLine());
+                        }
+                        else
+                        {
+                            for (int i = 0; i < numbers.Length; i++)
+                            {
+                                tempNumbers[i] = numbers[i];
+                            }
+                            Console.Write("Введите число: ");
+                            tempNumbers[tempNumbers.Length - 1] = Convert.ToInt32(Console.ReadLine());
+                            numbers = tempNumbers;
+                        }
                         break;
                     case "summ":
+                        for(int i = 0; i < numbers.Length; i++)
+                        {
+                            Console.Write(numbers[i] + " + ");
+                            summ += numbers[i];
+                        }
+                        Console.WriteLine(" = " + summ);
+                        Console.ReadKey();
                         break;
                     case "exit":
                         isWork = false;
                         break;
                 }
-                    
+                Console.Clear();
             }
-            
         }
     }
 }
