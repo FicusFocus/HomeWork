27e26ae66027e3803a0111e6c640798b8e4ec141
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2020-07-02 04:05:00 +0600

    HomeWork_4_1

--- HomeWork_4_1/Program.cs
+++ HomeWork_4_1/Program.cs
@@ -6,24 +6,57 @@ namespace HomeWork_4_1
     {
         static void Main(string[] args)
         {
-            int i;
             bool isWork = true;
-            string[] fullName = new string[1];
-            string[] position = new string[1];
+            string[] fullName = new string[0];
+            string[] position = new string[0];
 
             while (isWork)
             {
-                Console.WriteLine("Выберите действие: ");
+                Console.WriteLine("Выберите действие: \n" +
+                                  "1) addFile - добавить досье.\n" +
+                                  "2) showAll - показать список.\n" +
+                                  "3) deleteFile - удолить досье.\n" +
+                                  "4) findFile - найти досье по фамилии.\n" +
+                                  "5) exit - закрыть программу.");
 
-                switch 
+                switch (Console.ReadLine())
                 {
-                    case 1:
+                    case "addFile":
+                        Console.Write("Введите фамилию и имя: ");
+                        fullName = AddFile(fullName);
+                        Console.WriteLine("\nВведите должность: ");
+                        position = AddFile(position);
+                        break;
+                    case "showAll":
+                        for (int i = 0; i < fullName.Length; i++)
+                        {
+                            Console.WriteLine((i + 1) + ") " + fullName[i] + " - " + position[i]);
+                        }
+                        break;
+                    case "findFile":
+                        Console.Write("Введите фамилию и имя человека которого хотите найти: ");
+                        string name;
+                        name = Console.ReadLine();
+                        for (int i = 0; i < fullName.Length; i++)
+                        {
+                            if (name == fullName[i])
+                            {
+                                Console.Write(fullName[i] + " - " + position[i]);
+                            }
+                        }
+                        break;
+                    case "deleteFile":
+                        break;
+                    case "exit":
+                        isWork = false;
                         break;
                 }
+                Console.ReadKey();
+                Console.Clear();
             }
         }
 
-        static void AddFile(string[] array)
+        static string[] AddFile(string[] array)
         {
             string[] tempArrey = new string[array.Length + 1];
 
@@ -33,6 +66,7 @@ namespace HomeWork_4_1
             }
             tempArrey[tempArrey.Length - 1] = Console.ReadLine();
             array = tempArrey;
+            return array;
         }
     }
 }
