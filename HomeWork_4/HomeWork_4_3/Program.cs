3cda1da4c1215db2f3f8e58662b5ba5fdf635ac4
FicusFocus <shkurihin.pashok@mail.ru>
2020-07-05 00:28:22 +0600

    HomeWork_4_3

---
+++ HomeWork_4/HomeWork_4_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HomeWork_4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetValue();
+        }
+
+        static int GetValue()
+        {
+            int value;
+            string str;
+
+
+            while (true)
+            {
+                Console.Write("Введите число: ");
+                str = Console.ReadLine();
+                bool result = int.TryParse(str, out value);
+
+                if (result == true)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Введенное число - {value}");
+                    return value;
+                }
+                else
+                {
+                    Console.Write("\nНекорректно введено число, аовторите попытку ");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
