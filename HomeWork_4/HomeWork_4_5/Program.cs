4bdb1fa4863754f4b63dab5db08ad1b99fb9412e
FicusFocus <shkurihin.pashok@mail.ru>
2020-07-10 21:58:40 +0600

    HomeWork_4_5

--- HomeWork_4/HomeWork_4_5/Program.cs
+++ HomeWork_4/HomeWork_4_5/Program.cs
@@ -9,16 +9,24 @@ namespace HomeWork_4_5
             int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             Shuffle(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ,");
+            }
         }
 
         static void Shuffle(int[] array)
         {
             Random rnd = new Random();
-            int j = rnd.Next(0, array.Length - 1);
 
-            for(int i = 0; i < array.Length; i++)
+            for (int i = array.Length - 1; i >= 1; i--)
             {
-                
+                int j = rnd.Next(i + 1);
+
+                int temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
             }
         }
     }
