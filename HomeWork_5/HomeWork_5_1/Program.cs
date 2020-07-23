f50bebae813edeab50e8e21e51bbdc31866cfb11
FicusFocus <shkurihin.pashok@mail.ru>
2020-07-23 23:20:22 +0600

    HomeWork_5_1

--- HomeWork_5/HomeWork_5_1/Program.cs
+++ HomeWork_5/HomeWork_5_1/Program.cs
@@ -7,8 +7,31 @@ namespace HomeWork_5_1
     {
         static void Main(string[] args)
         {
-            Dictionary<int, string> asd = new Dictionary<int, string>();
-            asd.Add(1, "one");
+            string userInput;
+
+            Dictionary<string, int> numbering = new Dictionary<string ,int>();
+            numbering.Add("one", 1);
+            numbering.Add("two", 2);
+            numbering.Add("three", 3);
+            numbering.Add("four", 4);
+
+            while (true)
+            {
+                Console.WriteLine("Введите слово ключ.");
+                userInput = Console.ReadLine();
+
+                if (numbering.ContainsKey(userInput))
+                {
+                Console.WriteLine(numbering[userInput]);
+                }
+                else
+                {
+                    Console.WriteLine("Данного слова не обнаружено");
+                }
+                Console.ReadLine();
+                Console.Clear();
+            }
         }
+
     }
 }
