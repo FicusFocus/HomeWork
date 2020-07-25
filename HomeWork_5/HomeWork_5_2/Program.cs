860a59c49f8a025c5cd779060f8617e77832accc
FicusFocus <shkurihin.pashok@mail.ru>
2020-07-25 15:58:40 +0600

    HomeWork_5_2

---
+++ HomeWork_5/HomeWork_5_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HomeWork_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money = 0;
+            int buyAmount = 0, purchaseNumber;
+            Random rnd = new Random();
+
+            Queue<string> buyer = new Queue<string>();
+
+            buyer.Enqueue("buyer №1");
+            buyer.Enqueue("buyer №2");
+            buyer.Enqueue("buyer №3");
+            buyer.Enqueue("buyer №4");
+            buyer.Enqueue("buyer №5");
+            buyer.Enqueue("buyer №6");
+
+            purchaseNumber = rnd.Next(1, 6);
+
+            for (int i = 0; i < purchaseNumber; i++)
+            {
+                buyAmount = rnd.Next(1, 500);
+                money += buyAmount;
+
+                Console.WriteLine(buyAmount);
+                Console.WriteLine(money);
+            }
+        }
+    }
+}
