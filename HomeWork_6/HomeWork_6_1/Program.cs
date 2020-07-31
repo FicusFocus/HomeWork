d3d7b4dfe0b303fd48f2fe116515be34d654b0bb
FicusFocus <shkurihin.pashok@mail.ru>
2020-08-01 00:25:34 +0600

    HomeWork_6_1

---
+++ HomeWork_6/HomeWork_6_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeWork_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player();
+            player1.ShowStats("Bananza", 1000, 15, 150);
+        }
+    }
+
+    class Player
+    {
+        private string _name;
+        private int _helth;
+        private int _armor;
+        private int _damage;
+
+        public void ShowStats(string name, int helth, int armor, int damage)
+        {
+            _name = name;
+            _helth = helth;
+            _armor = armor;
+            _damage = damage;
+
+            Console.WriteLine($"имя - {_name}.\nздоровье - {_helth}.\nброня - {_armor}.\nурон - {_damage}.");
+        }
+    }
+}
