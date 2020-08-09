76f047c8c148586e9aef994e1b789df520a8916a
FicusFocus <shkurihin.pashok@mail.ru>
2020-08-10 03:22:43 +0600

    HomeWork_6_4

---
+++ HomeWork_6/HomeWork_6_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HomeWork_6_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Seller seller = new Seller();
+            Buyer buyer = new Buyer();
+            Product[] products = new Product[];
+
+
+        }
+    }
+
+    class Seller
+    {
+        private int _sellerMoney;
+    }
+
+    class Buyer
+    {
+        private int _buyerMoney;
+    }
+
+    class Product
+    {
+        private int _productNumber;
+        public int Price;
+        public string Name;
+
+        public Product(int productNumber, string name, int price)
+        {
+            _productNumber = productNumber;
+            Name = name;
+            Price = price;
+        }
+    }
+}
