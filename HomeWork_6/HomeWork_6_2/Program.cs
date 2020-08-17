f27cd6fa91a2e1b1121716d682b67dd3f72c4260
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2020-08-17 09:46:59 +0600

    HomeWork_6_2

---
+++ HomeWork_6/HomeWork_6_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Dynamic;
+
+namespace HomeWork_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(7, 7);
+            Renderer render = new Renderer();
+
+            render.DrowPosition(player.PositionX, player.PositionY);
+        }
+    }
+
+    class Player
+    {
+        public int PositionX { get; private set; }
+        public int PositionY { get; private set; }
+
+        public Player(int x, int y)
+        {
+            PositionX = x;
+            PositionY = y;
+        }
+    }
+
+    class Renderer
+    {
+        public void DrowPosition(int x, int y, char positionSimbool = 'Ж')
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(positionSimbool);
+        }
+    }
+}
---
+++ HomeWork_6/HomeWork_6_3/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace HomeWork_6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            List<Player> players = new List<Player>();
+
+            while (isWork)
+            {
+                Console.WriteLine("1 - добавить игрока\n" +
+                                  "2 - поиск игрока\n" +
+                                  "3 - вывести список игроков\n" +
+                                  "4 - забанить игрока\n" +
+                                  "5 - разбанить игрока\n" +
+                                  "6 - удалить игрока\n" +
+                                  "7 - выход");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.Write("Введите имя персонажа: ");
+                        string name = Console.ReadLine();
+                        Console.Write("\nВведите уровень персонажа: ");
+                        int lvl = Convert.ToInt32(Console.ReadLine());
+
+                        players.Add(new Player(name, lvl));
+                        break;
+                    case "2":
+                        Console.WriteLine("Поиск по номеру - 1, поиско по имени - 2: ");
+                        switch (Console.ReadLine())
+                        {
+                            case "1":
+                                Console.Write("Введите порядковый номер персонажа: ");
+                                int number = Convert.ToInt32(Console.ReadLine());
+                                
+                                break;
+                             case "2":
+                                break;
+                        }
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        /////////
+        public void FindPlayer(List<Player> players, int number)
+        {
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (i == number)
+                {
+
+                }
+            }
+        }
+        /////////
+    }
+
+    class Player
+    {
+        private string _name;
+        private int _lvl;
+        private bool _flag;
+        public int Number { get; private set; }
+
+        public Player(string name, int lvl = 1, bool flag = true)
+        {
+            _name = name;
+            _lvl = lvl;
+            _flag = flag;
+        }
+
+        public void FindPlayer(List<Player> players, int number)
+        {
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (i == number)
+                    Number = number;
+            }
+        }
+    }
+}
