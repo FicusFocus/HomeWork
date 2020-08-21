07e5aff1509ed196100fab51c833d79fa46a9c91
FicusFocus <shkurihin.pashok@mail.ru>
2020-08-21 17:02:35 +0600

    HomeWork_6_3
    Add FindPlayer

--- HomeWork_6/HomeWork_6_3/Program.cs
+++ HomeWork_6/HomeWork_6_3/Program.cs
@@ -8,6 +8,7 @@ namespace HomeWork_6_3
         static void Main(string[] args)
         {
             bool isWork = true;
+            int number;
 
             List<Player> players = new List<Player>();
 
@@ -28,64 +29,30 @@ namespace HomeWork_6_3
 
                         players.Add(new Player(players.Count + 1, playerName, 1));
                         break;
-
                     case "2":
                         Console.Write("Введите номер игрока которого желаете найти: ");
-                        int number = Convert.ToInt32(Console.ReadLine()) - 1;
-
-                        if (number > players.Count || number < 0)
-                        {
-                            Console.WriteLine("Игрока с таким номером не существует.");
-                            break;
-                        }
-                        players[number].ShowInfo();
+                        FindPlayer(players, out number);
+                        players[number - 1].ShowInfo();
                         break;
 
                     case "3":
-                        Console.WriteLine("Введите номер игрока которого желаете найти: ");
-                        number = Convert.ToInt32(Console.ReadLine()) - 1;
-                        if (number > players.Count || number < 0)
-                        {
-                            Console.WriteLine("Игрока с таким номером не существует.");
-                            break;
-                        }
                         for (int i = 0; i < players.Count; i++)
                             players[i].ShowInfo();
                         break;
-
                     case "4":
                         Console.WriteLine("Введите номер игрока которого желаете забанить: ");
-                        number = Convert.ToInt32(Console.ReadLine()) - 1;
-                        if (number > players.Count || number < 0)
-                        {
-                            Console.WriteLine("Игрока с таким номером не существует.");
-                            break;
-                        }
-                        else
-                            players[number].Flag = false;
+                        FindPlayer(players, out number);
+                        players[number - 1].Flag = false;
                         break;
                     case "5":
                         Console.WriteLine("Введите номер игрока которого желаете разбанить: ");
-                        number = Convert.ToInt32(Console.ReadLine()) - 1;
-                        if (number > players.Count || number < 0)
-                        {
-                            Console.WriteLine("Игрока с таким номером не существует.");
-                            break;
-                        }
-                        else
-                            players[number].Flag = true;
+                        FindPlayer(players, out number);
+                        players[number - 1].Flag = true;
                         break;
                     case "6":
                         Console.WriteLine("Введите номер игрока которого желаете удалить: ");
-                        number = Convert.ToInt32(Console.ReadLine()) - 1;
-                        if (number > players.Count || number < 0)
-                        {
-                            Console.WriteLine("Игрока с таким номером не существует.");
-                            break;
-                        }
-                        else
-                            players.RemoveAt(number);
-
+                        FindPlayer(players, out number);
+                        players.RemoveAt(number);
                             break;
                     case "7":
                         isWork = false;
@@ -96,20 +63,39 @@ namespace HomeWork_6_3
                 Console.Clear();
             }
         }
+
+        static void FindPlayer(List<Player> players, out int number)
+        {
+            bool isCorrect = true;
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number > players.Count || number < 0)
+            {
+                while (isCorrect)
+                {
+                    if (number > players.Count || number < 0)
+                    {
+                        Console.WriteLine("Игрока с таким номером не существует. Введите номер повторно.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                    }
+                    else
+                        isCorrect = false;
+                }
+            }
+        }
     }
 
     class Player
     {
-        public int Number;
-        public string Name;
-        public int Lvl;
+        private int _number;
+        private string _name;
+        private int _lvl;
         public bool Flag;
 
         public Player(int number, string name, int lvl, bool flag = true)
         {
-            Number = number;
-            Name = name;
-            Lvl = lvl;
+            _number = number;
+            _name = name;
+            _lvl = lvl;
             Flag = flag;
         }
 
