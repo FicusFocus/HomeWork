8d979cbd5dabf38a45d633370fcc454baf7a5158
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2021-05-25 06:25:20 +0600

    .

--- HomeWork_6/HomeWork_6_11_CarService/Program.cs
+++ HomeWork_6/HomeWork_6_11_CarService/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 //У вас есть автосервис, в который приезжают люди, чтобы починить свои автомобили.
 //У вашего автосервиса есть баланс денег и склад деталей.
@@ -15,13 +16,87 @@ namespace HomeWork_6_11_CarService
     {
         static void Main(string[] args)
         {
+            int carsAmount = 80;
+
+            CarService carService = new CarService();
+            Random rand = new Random();
+
+            while (carsAmount > 0)
+            {
+                Car car = new Car();
+                Console.WriteLine("В автосервис на ремонт прибыла новая машина");
+
+                string brokenPartName = null;
+                int brokenPartNumber = rand.Next(0, carService.PartsAmount);
+                int brokenPartPrice = 0;
+
+                if (carService.CheckSparePartAvailabiliti(brokenPartNumber, ref brokenPartName, ref brokenPartPrice))
+                {
+
+                }
+
+                Console.ReadLine();
+                Console.Clear();
+            }
         }
     }
 
-    class Car { }
+    class CarService
+    {
+        private List<Storage> _spareParts = new List<Storage>();
+        private int _mone;
 
-    class CarServis { }
+        public int PartsAmount { get; private set; }
 
-    class Сustomer { }
+        public CarService()
+        {
+            _spareParts.Add(new Storage("Двигатель", 700, 350, 30));
+            _spareParts.Add(new Storage("Коробка передач", 600, 400, 30));
+            _spareParts.Add(new Storage("Проводка",300 ,300 ,50));
+            _spareParts.Add(new Storage("Аккумулятор", 100, 10, 50));
 
+            PartsAmount = _spareParts.Count;
+        }
+
+        public bool CheckSparePartAvailabiliti(int sparePartNumber, ref string sparePartName, ref int sparePartPrice)
+        {
+
+
+
+            return true;
+        }
+    }
+
+    class Car
+    {
+        public SparePart BrokenPart { get; private set; }
+        
+
+    }
+
+    class Storage
+    {
+        public SparePart SparePart { get; private set; }
+        public int Amount { get; private set; }
+        public int PriceForRepairs { get; private set; }
+
+        public Storage(string name, int price, int priceForRepairs, int amount)
+        {
+            SparePart = new SparePart(name, price);
+            PriceForRepairs = priceForRepairs;
+            Amount = amount;
+        }
+    }
+
+    class SparePart
+    {
+        public string Name { get; private set; }
+        public int Price { get; private set; }
+
+        public SparePart(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
 }
