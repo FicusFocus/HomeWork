f2c90cd37bf31cec8e11dbbc1c1e863d9cbdc4e2
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2021-06-14 01:14:35 +0600

    .

--- HoneWork_7/HomeWork_7_1_CriminalList/Program.cs
+++ HoneWork_7/HomeWork_7_1_CriminalList/Program.cs
@@ -10,76 +10,50 @@ namespace HomeWork_7_1_CriminalList
         {
             List<Criminal> criminals = new List<Criminal>();
             #region
-            criminals.Add(new Criminal("Осипов И.А.", true, 178, 85, Nationality.Русский));
-            criminals.Add(new Criminal("Чилищин Е.О.", false, 187, 90, Nationality.Русский));
-            criminals.Add(new Criminal("Иваненко И.П.", true, 160, 93, Nationality.Украинец));
-            criminals.Add(new Criminal("Феактистов А.А.", false, 210, 115, Nationality.Русский));
-            criminals.Add(new Criminal("Калинин П.А", false, 192, 100, Nationality.Русский));
-            criminals.Add(new Criminal("Угорелов А.Ю", true, 175, 70, Nationality.Русский));
-            criminals.Add(new Criminal("Шабанов А.А.", true, 183, 68, Nationality.Русский));
-            criminals.Add(new Criminal("Есенкулов Э.Е", true, 197, 105, Nationality.Грузин));
-            criminals.Add(new Criminal("Шевченко А.А", false, 169, 78, Nationality.Украинец));
-            criminals.Add(new Criminal("Ковалев И.И", false, 190, 80, Nationality.Русский));
-            criminals.Add(new Criminal("Мамедов М. М", true, 185, 80, Nationality.Грузин));
-            criminals.Add(new Criminal("Лучко В.И", false, 187, 90, Nationality.Украинец));
-            criminals.Add(new Criminal("Бондарев Д.А.", false, 188, 93, Nationality.Русский));
+            criminals.Add(new Criminal("Осипов И.А.", true, 178, 85, Nationality.Russian));
+            criminals.Add(new Criminal("Чилищин Е.О.", false, 187, 90, Nationality.Russian));
+            criminals.Add(new Criminal("Иваненко И.П.", true, 160, 93, Nationality.Ukrainian));
+            criminals.Add(new Criminal("Феактистов А.А.", false, 210, 115, Nationality.Russian));
+            criminals.Add(new Criminal("Калинин П.А", false, 192, 100, Nationality.Russian));
+            criminals.Add(new Criminal("Угорелов А.Ю", true, 175, 70, Nationality.Russian));
+            criminals.Add(new Criminal("Шабанов А.А.", true, 183, 68, Nationality.Russian));
+            criminals.Add(new Criminal("Есенкулов Э.Е", true, 197, 105, Nationality.Georgian));
+            criminals.Add(new Criminal("Шевченко А.А", false, 169, 78, Nationality.Ukrainian));
+            criminals.Add(new Criminal("Ковалев И.И", false, 190, 80, Nationality.Russian));
+            criminals.Add(new Criminal("Мамедов М. М", true, 185, 80, Nationality.Georgian));
+            criminals.Add(new Criminal("Лучко В.И", false, 187, 90, Nationality.Ukrainian));
+            criminals.Add(new Criminal("Бондарев Д.А.", false, 188, 93, Nationality.Russian));
             #endregion
 
             bool isWork = true;
 
             while (isWork)
             {
-                Console.WriteLine("По какому параметру желаете найти пресутпника:\n" +
-                                  "1) по росту\n" +
-                                  "2) по весу\n" +
-                                  "3) по национальности");
+                int height;
+                int weight;
+                int nattionalityNumber;
+                string userInput;
 
-                switch (Console.ReadKey().Key)
-                {
 
-                    case ConsoleKey.Escape:
-                        isWork = false;
-                        Console.WriteLine("Сеанс завершен.");
-                        break;
+                Console.WriteLine("Введите рост, вес и национальность:\n");
+                Console.Write("Введите рост: ");
+                userInput = Console.ReadLine();
+                var result = int.TryParse(userInput, out height);
+                Console.Write("\nВведите вес: ");
+                userInput = Console.ReadLine();
+                result = int.TryParse(userInput, out weight);
+                Console.Write($"\nВыберите национальность:\n1) - {(Nationality)1}\n2) - {(Nationality)2}\n3) {(Nationality)3}\n");
+                userInput = Console.ReadLine();
+                result = int.TryParse(userInput, out nattionalityNumber);
 
-                    case ConsoleKey.D1:
-                        Console.Write("Введите предпологаемый рост цели:");
+                var filteredCriminals = criminals.Where(criminal => criminal.Height == height &&
+                                                        criminal.Weight == weight &&
+                                                        criminal.Nationality == (Nationality)nattionalityNumber &&
+                                                        criminal.IsInCustody == false);
 
-                        int height;
-                        string input = Console.ReadLine();
-                        bool result = int.TryParse(input, out height);
+                foreach (var criminal in filteredCriminals)
+                    Console.WriteLine(criminal.FullName);
 
-                        var filteredCriminals = criminals.Where(criminal => criminal.Height == height).Where(criminal => criminal.IsInCustody == false);
-
-                        foreach (var criminal in filteredCriminals)
-                            Console.WriteLine(criminal.FullName);
-                        break;
-
-                    case ConsoleKey.D2:
-                        Console.Write("Введите предпологаемый вес цели:");
-
-                        int weigth;
-                        input = Console.ReadLine();
-                        result = int.TryParse(input, out weigth);
-                        filteredCriminals = criminals.Where(criminal => criminal.Weight == weigth).Where(criminal => criminal.IsInCustody == false);
-
-                        foreach (var criminal in filteredCriminals)
-                            Console.WriteLine(criminal.FullName);
-                        break;
-
-                    case ConsoleKey.D3:
-                        Console.WriteLine($"Выберите национальность:\n1) - {(Nationality)1}\n2) - {(Nationality)2}\n 3) {(Nationality)3}\n");
-                        
-                        int nattionalityNumber;
-                        input = Console.ReadLine();
-                        result = int.TryParse(input, out nattionalityNumber);
-                        filteredCriminals = criminals.Where(criminal => (criminal.Nationality == (Nationality)nattionalityNumber)).Where(criminal => criminal.IsInCustody == false);
-
-                        foreach (var criminal in filteredCriminals)
-                            Console.WriteLine(criminal.FullName);
-
-                        break;
-                }
 
                 Console.ReadLine();
                 Console.Clear();
@@ -89,9 +63,9 @@ namespace HomeWork_7_1_CriminalList
 
     enum Nationality
     {
-        Русский = 1,
-        Украинец,
-        Грузин
+        Russian = 1,
+        Ukrainian,
+        Georgian
     }
 
     class Criminal
