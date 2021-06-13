451eee7d35930432994201b6e3680c117803d6b2
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2021-06-14 04:35:41 +0600

    HomeWork_7_3

--- HoneWork_7/HomeWork_7_3_AnarchyInTheHospital/Program.cs
+++ HoneWork_7/HomeWork_7_3_AnarchyInTheHospital/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 //У вас есть список больных(минимум 10 записей)
 //Класс больного состоит из полей: ФИО, возраст, заболевание.
@@ -18,42 +16,92 @@ namespace HomeWork_7_3_AnarchyInTheHospital
     {
         static void Main(string[] args)
         {
-            List<Patient> criminals = new List<Patient>();
+            List<Patient> patients = new List<Patient>();
             #region
-            criminals.Add(new Patient("Осипов И.А.", Disease.AntiGovernment));
-            criminals.Add(new Patient("Чилищин Е.О.", Disease.Vandalism));
-            criminals.Add(new Patient("Иваненко И.П.", Disease.AntiGovernment));
-            criminals.Add(new Patient("Феактистов А.А.", Disease.Vandalism));
-            criminals.Add(new Patient("Калинин П.А", Disease.AntiGovernment));
-            criminals.Add(new Patient("Угорелов А.Ю", Disease.Vandalism));
-            criminals.Add(new Patient("Шабанов А.А.", Disease.Murder));
-            criminals.Add(new Patient("Есенкулов Э.Е", Disease.Murder));
-            criminals.Add(new Patient("Шевченко А.А", Disease.AntiGovernment));
-            criminals.Add(new Patient("Ковалев И.И", Disease.Vandalism));
-            criminals.Add(new Patient("Мамедов М. М", Disease.Murder));
-            criminals.Add(new Patient("Лучко В.И", Disease.AntiGovernment));
-            criminals.Add(new Patient("Бондарев Д.А.", Disease.Vandalism));
+            patients.Add(new Patient("Осипов И.А.", 25, Disease.influenza));
+            patients.Add(new Patient("Чилищин Е.О.", 18, Disease.quinsy));
+            patients.Add(new Patient("Иваненко И.П.", 45, Disease.influenza));
+            patients.Add(new Patient("Феактистов А.А.", 25, Disease.quinsy));
+            patients.Add(new Patient("Калинин П.А", 18, Disease.allergy));
+            patients.Add(new Patient("Угорелов А.Ю", 40, Disease.quinsy));
+            patients.Add(new Patient("Шабанов А.А.", 30, Disease.diabetes));
+            patients.Add(new Patient("Есенкулов Э.Е", 25, Disease.diabetes));
+            patients.Add(new Patient("Шевченко А.А", 20, Disease.allergy));
+            patients.Add(new Patient("Ковалев И.И", 30, Disease.quinsy));
+            patients.Add(new Patient("Мамедов М. М", 50, Disease.diabetes));
+            patients.Add(new Patient("Лучко В.И", 45, Disease.influenza));
+            patients.Add(new Patient("Бондарев Д.А.", 18, Disease.quinsy));
             #endregion
+
+            bool isWork = true;
+
+            while (isWork)
+            {
+                Console.WriteLine("               БОЛЬНИЦА");
+                Console.WriteLine("Выберите по какому параметру желаете отсортировать больных:\n" +
+                                  "1)Отсортировать всех больных по фио\n" +
+                                  "2)Отсортировать всех больных по возрасту\n" +
+                                  "3)Вывести больных с определенным заболеванием");
+
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.D1:
+                        var filteredByFullName = patients.OrderBy(patient => patient.Fullname);
+
+                        foreach (var patient in filteredByFullName)
+                            patient.ShowInfo();
+
+                        break;
+
+                    case ConsoleKey.D2:
+                        var filteredByAge = patients.OrderBy(patient => patient.Age);
+
+                        foreach (var patient in filteredByAge)
+                            patient.ShowInfo();
+
+                        break;
+
+                    case ConsoleKey.D3:
+                        Console.Write("Введите название заболевания:");
+                        string userInput = Console.ReadLine().ToLower();
+
+                        var filteredByDisease = patients.Where(patient => patient.DiseaseName.ToString() == userInput);
+
+                        foreach (var patient in filteredByDisease)
+                            patient.ShowInfo();
+
+                        break;
+                }
+
+                Console.ReadLine();
+                Console.Clear();
+            }
         }
     }
 
     class Patient
     {
         public string Fullname { get; private set; }
-        public bool IsInCustody { get; private set; }
+        public int Age { get; private set; }
         public Disease DiseaseName { get; private set; }
-        public Patient(string fullName, Disease criminalTip)
+        public Patient(string fullName, int age, Disease diseaseName)
         {
-            this.DiseaseName = criminalTip;
+            this.DiseaseName = diseaseName;
             Fullname = fullName;
-            IsInCustody = true;
+            Age = age;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"\n{Fullname} - {Age}лет, заболевание - {DiseaseName}");
         }
     }
 
     enum Disease
     {
-        AntiGovernment,
-        Vandalism,
-        Murder
+        influenza,
+        quinsy,
+        diabetes,
+        allergy
     }
 }
