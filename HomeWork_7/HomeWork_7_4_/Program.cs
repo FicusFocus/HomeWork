9990aa458502266b7b20bdd8747e8d8b08d7f62e
FicusFocus <shkurihin.pashok@mail.ru>
2021-06-16 07:22:23 +0600

    .

---
+++ HomeWork_7/HomeWork_7_4_/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//У нас есть список всех игроков(минимум 10). 
+//У каждого игрока есть поля: имя, уровень, сила.
+//Требуется написать запрос для определения топ 3 игроков по уровню и топ 3 игроков по силе, после чего вывести каждый топ.
+//2 запроса получится.
+
+namespace HomeWork_7_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
