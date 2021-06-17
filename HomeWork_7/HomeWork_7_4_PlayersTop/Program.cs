bae6bc6526a375dae3db5bb3f690d8cc47eced0e
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2021-06-17 06:29:28 +0600

    /

--- HomeWork_7/HomeWork_7_4_PlayersTop/Program.cs
+++ HomeWork_7/HomeWork_7_4_PlayersTop/Program.cs
@@ -13,7 +13,34 @@ namespace HomeWork_7_4_PlayersTop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Player> players = new List<Player>();
+
+            players.Add(new Player("SantaKlauss", 83, 220));
+            players.Add(new Player("ZombI228", 85, 222));
+            players.Add(new Player("БурунБык", 73, 200));
+            players.Add(new Player("Снедурочка", 90, 190));
+            players.Add(new Player("kleoPETR", 50, 100));
+            players.Add(new Player("InKYBaToR", 88, 210));
+            players.Add(new Player("KlirrrIk", 60, 110));
+            players.Add(new Player("StreloK", 81, 200));
+            players.Add(new Player("KoloBloK", 90, 150));
+            players.Add(new Player("Капатыч", 79, 201));
+            players.Add(new Player("Купесь", 85, 193));
+
+        }
+    }
+
+    class Player
+    {
+        public string  Name { get; private set; }
+        public int Lvl { get; private set; }
+        public int Strength { get; private set; }
+
+        public Player(string name, int lvl, int strength)
+        {
+            name = name;
+            Lvl = lvl;
+            Strength = strength;
         }
     }
 }
