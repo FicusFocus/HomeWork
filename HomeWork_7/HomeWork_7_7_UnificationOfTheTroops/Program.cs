6a65c397d488a2f615ced6d96434bcdbe428068d
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2021-06-18 03:32:48 +0600

    .

---
+++ HomeWork_7/HomeWork_7_7_UnificationOfTheTroops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//Есть 2 списка в солдатами.
+//Всех бойцов из отряда 1, у которых фамилия начинается на букву Б, требуется перевести в отряд 2
+
+namespace HomeWork_7_7_UnificationOfTheTroops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Soldier
+    {
+
+    }
+}
